94643b41bb2b2f49bbaf63c18b3787aec9fec032
Paulo Pitte <paulopitte@gmail.com>
2022-02-14 17:56:04 -0300

    Adicionar arquivos de projeto.

---
+++ MassTransitInMemoryBus/MassTransitInMemoryService.cs
@@ -0,0 +1,55 @@
+﻿using MassTransit;
+
+namespace MassTransitInMemoryBus
+{
+     static class MassTransitInMemoryService
+    {
+        public static async void Process()
+        {
+            var bus = Bus.Factory.CreateUsingInMemory(sbc =>
+            {
+                sbc.ReceiveEndpoint("order_queue", ep =>
+                {
+                    ep.Handler<Message>(context =>
+                    {
+                        return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                    });
+                });
+            });
+
+            await bus.StartAsync();
+
+            try
+            {
+                var index = 0;
+                while (true)
+                {
+
+                    object p = bus.Publish(new Message
+                    {
+                        OrderId = index,
+                        Text = $"{DateTime.UtcNow} => message index order: {index++}"
+                    });
+
+                    await Task.Run(() => Thread.Sleep(1000));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex}");
+            }
+            finally
+            {
+                await bus.StopAsync();
+            }
+        }
+
+
+        public class Message
+        {
+            public int OrderId { get; set; }
+            public string Text { get; set; }
+        }
+
+    }
+}
---
+++ MassTransitInMemoryBus/Program.cs
@@ -0,0 +1,9 @@
+﻿using MassTransitInMemoryBus;
+
+Console.WriteLine("MassTransit in Memory!");
+
+MassTransitInMemoryService.Process();
+
+Console.ReadKey();
+
+
